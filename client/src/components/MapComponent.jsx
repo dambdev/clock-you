9cc4cc86161e7754e53a2d28c4ec8919e4ac55db
David Mirás <david_miras@icloud.com>
2024-10-11 15:38:14 +0200

    Retoques

--- client/src/components/MapComponent.jsx
+++ client/src/components/MapComponent.jsx
@@ -21,7 +21,7 @@ const MapComponent = ({ location }) => {
     if (exitLocation) positions.push(exitLocation);
 
     return (
-        <MapContainer center={mapCenter} zoom={16} scrollWheelZoom={false}>
+        <MapContainer center={mapCenter} zoom={15} scrollWheelZoom={false}>
             <TileLayer
                 attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
                 url='https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png'
