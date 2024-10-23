2cc6450980a23b0ab2cd0a072d552a13b0d5761c
David Mirás <david_miras@icloud.com>
2024-10-23 23:53:04 +0200

    Mejoras

--- client/src/pages/HomePage.jsx
+++ client/src/pages/HomePage.jsx
@@ -1,3 +1,4 @@
+import { NavLink } from 'react-router-dom';
 import ContactFormComponent from '../components/ContactFormComponent.jsx';
 
 const { VITE_APP_TITLE } = import.meta.env;
@@ -16,16 +17,23 @@ const HomePage = () => {
                 <div>
                     <h2>¿Cómo funciona?</h2>
                     <div className='articles-container'>
-                        <article className='mt-6'>
-                            <img src='./step-1-pShop.webp' alt='' />
+                        <NavLink to={'/typeOfServices'}>
+                            <article className='mt-6'>
+                                <img
+                                    src='./step-1-pShop.webp'
+                                    alt='Ver servicios'
+                                    className='cursor-pointer'
+                                />
+
+                                <p className='text-center'>
+                                    Tenemos casi de todo: limpieza a domicilio,
+                                    clases particulares, cuidado de mascotas...
+                                </p>
+                            </article>
                             <h3>Busca lo que necesitas</h3>
-                            <p className='text-center'>
-                                Tenemos casi de todo: limpieza a domicilio,
-                                clases particulares, cuidado de mascotas...
-                            </p>
-                        </article>
+                        </NavLink>
                         <article className='mt-6'>
-                            <img src='./step-2.webp' alt='' />
+                            <img src='./step-2.webp' alt='Empleados' />
                             <h3>El profesional ideal</h3>
                             <p className='text-center'>
                                 De nuestra amplia variedad, seleccionaremos al
