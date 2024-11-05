9f7136ed91defd7ece28d9dd4f3a70ee5eebd994
david <david_miras@icloud.com>
2024-11-06 00:14:58 +0100

    corrección

--- server/src/db/getPool.js
+++ server/src/db/getPool.js
@@ -1,24 +1,20 @@
 import mysql from 'mysql2/promise';
-import {
-    MYSQL_HOST,
-    MYSQL_USER,
-    MYSQL_PASS,
-    MYSQL_DB,
-    MYSQL_PORT,
-} from '../../env.js';
+import { MYSQL_HOST, MYSQL_USER, MYSQL_PASS, MYSQL_DB } from '../../env.js';
 
 let pool;
 
 const getPool = async () => {
     try {
-        pool = mysql.createPool({
-            host: MYSQL_HOST,
-            user: MYSQL_USER,
-            password: MYSQL_PASS,
-            database: MYSQL_DB,
-            port: MYSQL_PORT,
-            timezone: 'Z',
-        });
+        if (!pool) {
+            pool = mysql.createPool({
+                connectionLimit: 10,
+                host: MYSQL_HOST,
+                user: MYSQL_USER,
+                password: MYSQL_PASS,
+                database: MYSQL_DB,
+                timezone: 'Z',
+            });
+        }
 
         return pool;
     } catch (err) {
