7e020df62443e40f24c3285673de699d9abbacd6
david <david_miras@icloud.com>
2024-11-08 21:49:05 +0100

    puertos

--- server/app.js
+++ server/app.js
@@ -30,7 +30,7 @@ app.use(notFoundErrorController);
 
 app.use(errorController);
 
-const PORT = process.env.PORT || 8000;
+const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
     console.log(`Server running on port http://localhost:${PORT}`);
