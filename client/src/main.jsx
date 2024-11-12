48db32d692f95f1d7e124bc46f8d7250988200d7
david <david_miras@icloud.com>
2024-11-12 20:03:56 +0100

    correcciones

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,7 +1,6 @@
 import App from './App.jsx';
 import Modal from 'react-modal';
 import { Toaster } from 'react-hot-toast';
-import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { AuthProvider } from './context/AuthContext.jsx';
 import { BrowserRouter } from 'react-router-dom';
@@ -9,23 +8,21 @@ import { BrowserRouter } from 'react-router-dom';
 Modal.setAppElement('#root');
 
 createRoot(document.getElementById('root')).render(
-    <StrictMode>
-        <BrowserRouter>
-            <AuthProvider>
-                <App />
-                <Toaster
-                    position='top-center'
-                    toastOptions={{
-                        duration: 7000,
-                        style: {
-                            backgroundColor: 'var(--cerulean)',
-                            color: 'var(--white-color)',
-                            borderRadius: '100px',
-                            textAlign: 'center',
-                        },
-                    }}
-                />
-            </AuthProvider>
-        </BrowserRouter>
-    </StrictMode>
+    <BrowserRouter>
+        <AuthProvider>
+            <App />
+            <Toaster
+                position='top-center'
+                toastOptions={{
+                    duration: 7000,
+                    style: {
+                        backgroundColor: 'var(--cerulean)',
+                        color: 'var(--white-color)',
+                        borderRadius: '100px',
+                        textAlign: 'center',
+                    },
+                }}
+            />
+        </AuthProvider>
+    </BrowserRouter>
 );
