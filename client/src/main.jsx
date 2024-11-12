a875aee013f359c22bd00fd471dc969e4552b5d7
david <david_miras@icloud.com>
2024-11-12 21:56:32 +0100

    error

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -8,25 +8,6 @@ import { BrowserRouter } from 'react-router-dom';
 Modal.setAppElement('#root');
 
 createRoot(document.getElementById('root')).render(
-<<<<<<< HEAD
-        <BrowserRouter>
-            <AuthProvider>
-                <App />
-                <Toaster
-                    position='top-center'
-                    toastOptions={{
-                        duration: 7000,
-                        style: {
-                            backgroundColor: 'var(--cerulean)',
-                            color: 'var(--white-color)',
-                            borderRadius: '100px',
-                            textAlign: 'center',
-                        },
-                    }}
-                />
-            </AuthProvider>
-        </BrowserRouter>
-=======
     <BrowserRouter>
         <AuthProvider>
             <App />
@@ -44,5 +25,4 @@ createRoot(document.getElementById('root')).render(
             />
         </AuthProvider>
     </BrowserRouter>
->>>>>>> 48db32d692f95f1d7e124bc46f8d7250988200d7
 );
